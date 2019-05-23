178030a176d71c86ec6aa0c36fcb32d0f4af19dc
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2019-05-23 09:41:00 -0700

    chore: add generic Node.js gitignore file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-/*
-9/22/2014 - Update 1: Added Author Name and changed some html and css. :) Enjoy.
-*/
+const $ = require('jquery');
+const ScrollReveal = require('scrollreveal');
+
+$(function() {
+
+    window.sr = ScrollReveal();
+
+    if ($(window).width() < 768) {
+
+        if ($('.conference-timeline-content').hasClass('js--fadeInLeft')) {
+            $('.conference-timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+        }
+
+        sr.reveal('.js--fadeInRight', {
+            origin: 'right',
+            distance: '300px',
+            easing: 'ease-in-out',
+            duration: 800,
+        });
+
+    } else {
+
+        sr.reveal('.js--fadeInLeft', {
+            origin: 'left',
+            distance: '300px',
+            easing: 'ease-in-out',
+            duration: 800,
+        });
+
+        sr.reveal('.js--fadeInRight', {
+            origin: 'right',
+            delay: 500,
+            distance: '300px',
+            easing: 'ease-in-out',
+            duration: 800,
+        });
+
+    }
+
+    sr.reveal('.js--fadeInLeft', {
+        origin: 'left',
+        distance: '300px',
+        easing: 'ease-in-out',
+        duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+        origin: 'right',
+        delay: 500,
+        distance: '300px',
+        easing: 'ease-in-out',
+        duration: 800,
+    });
+
+
+});
